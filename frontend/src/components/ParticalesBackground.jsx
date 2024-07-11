4452d5c7eaab99f6f91d00a779f521116ec7c798
Udaybabubojja <21pa1a0518@vishnu.edu.in>
2024-07-12 00:16:37 +0530

    Homepage updated!!

--- frontend/src/components/ParticalesBackground.jsx
+++ frontend/src/components/ParticalesBackground.jsx
@@ -1,11 +1,97 @@
-import Particles from 'react-tsparticles'
-import particalesConfig from "../config/paritcale-config"
-const ParticalesBackground = () => {
-  return (
-    <div>
-      <Particles id="tsparticles" params={particalesConfig} ></Particles>
-    </div>
-  )
-}
+import { useEffect, useMemo, useState } from "react";
+import { useColorMode } from "@chakra-ui/react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
-export default ParticalesBackground
+const ParticlesComponent = (props) => {
+  // eslint-disable-next-line no-unused-vars
+  const [init, setInit] = useState(false);
+  const { colorMode } = useColorMode();
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: colorMode === "dark" ? "#1A202C" : "#F7FAFC",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "repulse",
+          },
+          onHover: {
+            enable: true,
+            mode: "grab",
+          },
+        },
+        modes: {
+          push: {
+            distance: 200,
+            duration: 15,
+          },
+          grab: {
+            distance: 150,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: colorMode === "dark" ? "#FFFFFF" : "#000000",
+        },
+        links: {
+          color: colorMode === "dark" ? "#FFFFFF" : "#000000",
+          distance: 150,
+          enable: true,
+          opacity: 0.3,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: true,
+          speed: 1,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 150,
+        },
+        opacity: {
+          value: 1.0,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 3 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [colorMode]
+  );
+
+  return <Particles id={props.id} init={particlesLoaded} options={options} />;
+};
+
+export default ParticlesComponent;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,14 +3,19 @@ import { Link } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { FaUser, FaCompass } from 'react-icons/fa';
 import userAtom from '../atoms/userAtom';
-// import ThreeScene from '../components/ThreeScene';
-import '../styles/HomePage.css'; // Import CSS file for custom styling
+import ParticlesComponent from '../components/ParticalesBackground';
+import '../styles/HomePage.css';
+import Header from '../components/Header';
 
 const HomePage = () => {
   const user = useRecoilValue(userAtom);
 
   return (
     <div className="homepage-container">
+      <ParticlesComponent />
+      <div className="header">
+        <Header />
+      </div>
       <div className="content">
         <Link to={`/${user.user.username}`}>
           <Flex w={"full"} justifyContent={"center"} alignItems={"center"} mt={4}>
@@ -25,6 +30,6 @@ const HomePage = () => {
       </div>
     </div>
   );
-}
+};
 
 export default HomePage;
